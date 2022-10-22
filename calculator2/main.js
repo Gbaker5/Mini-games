f75a241e40c790bdc1dc81a0114a5834ce03a0f2
Gjarred Baker <gjarred23@gmail.com>
2022-10-22 08:05:10 -0500

    declare variables

---
+++ calculator2/main.js
@@ -0,0 +1,23 @@
+class Calculator{
+    constructor(prevOperandText, curOperandText){
+        
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const prevOperandText = document.querySelector('[data-prev-operand]')
+const curOperandText = document.querySelector('[data-current-operand]')
