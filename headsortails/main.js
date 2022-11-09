f1523f8917ac1cf5c879c4411e79188d0f30d19f
Gjarred Baker <gjarred23@gmail.com>
2022-11-09 08:05:10 -0600

    add event listeners

---
+++ headsortails/main.js
@@ -0,0 +1,12 @@
+//EVENT LISTENERS
+document.querySelector('#choiceButton').addEventListener('click', )
+document.querySelector('#heads').addEventListener('click', )
+document.querySelector('#tails').addEventListener('click', )
+document.querySelector('#result').addEventListener('click', )
+document.querySelector('#reset').addEventListener('click', )
+
+//VARIABLES
+
+
+
+//FUNCTIONS
