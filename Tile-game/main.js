a7a05908126fdaa97af17f2b8459fb45571ba553
Gjarred Baker <gjarred23@gmail.com>
2022-11-11 08:05:10 -0600

    functionality start

--- Tile-game/main.js
+++ Tile-game/main.js
@@ -7,7 +7,7 @@ var otherTile;//blank tile
 
 var turns = 0;
 
-var imgOrder = ["1","2","3","4","5","6","6","7","8","9"];//setting default image order
+var imgOrder = ["1","2","3","4","5","6","7","8","9"];//setting default image order
 
 window.onload = function(){ //when the page loads do this function
     for(let r=0;r < rows; r++){ //iterate through rows
@@ -16,10 +16,21 @@ window.onload = function(){ //when the page loads do this function
             //<img id="0-1">
             let tile = document.createElement("img");
             tile.id  = r.toString()+"-"+c.toString();//to identify current tile it will be decribed as "row number-column number"
-            tile.src = imgOrder.shift() + ".jpg";
-            document.getElementById("board").append(tile)
 
+            tile.src = imgOrder.shift() + ".jpg"; //gets the first number from the array and adds "jpg to the end" ex:"1.jpg, 2.jpg, etc"
+            
+            //DRAG FUNCTIONALITY
+            tile.addEventListener("dragstart", dragStart);
+            tile.addEventListener("dragover", dragOver);
+            tile.addEventListener("dragenter", dragEnter);
+            tile.addEventListener("dragLeave", dragLeave);
+            tile.addEventListener("drop", dragDrop);
+            tile.addEventListener("dragEnd", dragEnd);
+            
+            
+            document.getElementById("board").append(tile);//add images to board
 
+            
         }
     }
 
