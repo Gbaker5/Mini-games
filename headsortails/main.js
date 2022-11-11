e0be5a8b1734b4f7936460da8efc70b40b8f14b2
Gjarred Baker <gjarred23@gmail.com>
2022-11-10 22:02:44 -0600

    get choice function

--- headsortails/main.js
+++ headsortails/main.js
@@ -1,12 +1,39 @@
 //EVENT LISTENERS
-document.querySelector('#choiceButton').addEventListener('click', )
-document.querySelector('#heads').addEventListener('click', )
-document.querySelector('#tails').addEventListener('click', )
-document.querySelector('#result').addEventListener('click', )
-document.querySelector('#reset').addEventListener('click', )
+document.querySelector('#choiceButton').addEventListener('click', getChoice )
+document.querySelector('#heads').addEventListener('click', heads )
+document.querySelector('#tails').addEventListener('click', tails)
+document.querySelector('#resultButton').addEventListener('click', getResult )
+document.querySelector('#reset').addEventListener('click', reset )
 
 //VARIABLES
 
 
 
 //FUNCTIONS
+function getChoice(){
+    const choice = document.querySelector('#choice').value
+    console.log(choice)
+    if(choice === 'heads'){
+        document.querySelector('#choiceText').innerText = `You chose ${choice.toUpperCase()}!!!`
+    }else if(choice === 'tails'){
+        document.querySelector('#choiceText').innerText = `You chose ${choice.toUpperCase()}!!!`
+    }else{
+        return ''
+    }
+}
+
+function heads(){
+
+}
+
+function tails(){
+
+}
+
+function getResult(){
+
+}
+
+function reset(){
+
+}
