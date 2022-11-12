99120106d74a18827a6ea5b607fec46dcca427a8
Gjarred Baker <gjarred23@gmail.com>
2022-11-12 08:05:10 -0600

    functionality done

--- Tile-game/main.js
+++ Tile-game/main.js
@@ -20,12 +20,12 @@ window.onload = function(){ //when the page loads do this function
             tile.src = imgOrder.shift() + ".jpg"; //gets the first number from the array and adds "jpg to the end" ex:"1.jpg, 2.jpg, etc"
             
             //DRAG FUNCTIONALITY
-            tile.addEventListener("dragstart", dragStart);
-            tile.addEventListener("dragover", dragOver);
-            tile.addEventListener("dragenter", dragEnter);
-            tile.addEventListener("dragLeave", dragLeave);
-            tile.addEventListener("drop", dragDrop);
-            tile.addEventListener("dragEnd", dragEnd);
+            tile.addEventListener("dragstart", dragStart); //click an image to drag
+            tile.addEventListener("dragover", dragOver); //moving image around while clicked
+            tile.addEventListener("dragenter", dragEnter); //dragging image into another one
+            tile.addEventListener("dragLeave", dragLeave); //drag image out of another image
+            tile.addEventListener("drop", dragDrop); //drag image and release IT
+            tile.addEventListener("dragend", dragEnd); //after drag drop you swap tiles
             
             
             document.getElementById("board").append(tile);//add images to board
@@ -34,4 +34,38 @@ window.onload = function(){ //when the page loads do this function
         }
     }
 
+}
+
+function dragStart(){
+    currTile = this; //this refers to the img tile being dragged
+    console.log("started")
+}
+
+function dragOver(e){
+    e.preventDefault()
+    console.log("over")
+}
+
+function dragEnter(e){
+    e.preventDefault()
+    console.log("entered")
+}
+
+function dragLeave(e){
+    console.log("left")
+}
+
+function dragDrop(){
+   otherTile = this; //this refers to the img tile being dropped in
+   console.log("dropped")
+}
+
+function dragEnd(){
+    console.log("swapped")
+
+    let currImg = currTile.src;
+    let otherImg = otherTile.src;
+
+    currTile.src = otherImg;
+    otherTile.src = currImg;
 }
